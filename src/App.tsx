90840f414247b1e5ba8cad95379314d69546363f
Bryan McCarthy <bryanmccarthy2@gmail.com>
2024-01-05 20:35:42 +0000

    convert svg to jsx

--- src/App.tsx
+++ src/App.tsx
@@ -1,8 +1,13 @@
+import World from './components/World';
+
 function App() {
 
   return (
     <>
-      <div className="text-2xl font-bold text-center">Georacer</div>
+      <div className="h-[calc(100dvh)]">
+        <div className="h-[calc(5dvh)] text-2xl font-bold text-center">Header</div>
+        <World />
+      </div>
     </>
   )
 }
