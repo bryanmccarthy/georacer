5560b4fe42171aa81ddc904c7cfe9225a7ec9a7f
Bryan McCarthy <bryanmccarthy2@gmail.com>
2024-01-08 18:15:27 +0000

    wip

--- src/components/World.tsx
+++ src/components/World.tsx
@@ -1,19 +1,19 @@
-import { ComposableMap, Geographies, Geography } from "react-simple-maps"
+import { ComposableMap, Geographies, Geography } from 'react-simple-maps'
 import geoJSON from '../geo/countries.json'
 
 export default function World() {
   return (
     <>
-      <div className="">
-      <ComposableMap>
-      <Geographies geography={geoJSON}>
-        {({ geographies }) =>
-          geographies.map((geo) => (
-            <Geography key={geo.rsmKey} geography={geo} />
-          ))
-        }
-      </Geographies>
-      </ComposableMap>
+      <div className="w-full h-[calc(95dvh)] overflow-scroll">
+        <ComposableMap className="bg-blue-500">
+          <Geographies geography={geoJSON}>
+            {({ geographies }) =>
+              geographies.map((geo) => (
+                <Geography key={geo.rsmKey} geography={geo} />
+              ))
+            }
+          </Geographies>
+        </ComposableMap>
       </div>
     </>
   )
