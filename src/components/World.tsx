1393b22761e2e9d8d8eef47d8f4ef0ba457f471d
Bryan McCarthy <bryanmccarthy2@gmail.com>
2024-01-09 10:34:59 +0000

    add zoomable group

--- src/components/World.tsx
+++ src/components/World.tsx
@@ -1,18 +1,25 @@
-import { ComposableMap, Geographies, Geography } from 'react-simple-maps'
+import { ComposableMap, Geographies, Geography, ZoomableGroup } from 'react-simple-maps'
 import geoJSON from '../geo/countries.json'
 
 export default function World() {
+
+  const handleGeoClick = (geo: any) => {
+    console.log(geo)
+  }
+
   return (
     <>
       <div className="w-full h-[calc(95dvh)] overflow-scroll">
         <ComposableMap className="bg-blue-500">
-          <Geographies geography={geoJSON}>
-            {({ geographies }) =>
-              geographies.map((geo) => (
-                <Geography key={geo.rsmKey} geography={geo} />
-              ))
-            }
-          </Geographies>
+          <ZoomableGroup center={[0, 0]} zoom={1}>
+            <Geographies geography={geoJSON}>
+              {({ geographies }) =>
+                geographies.map((geo) => (
+                  <Geography key={geo.rsmKey} geography={geo} onClick={() => handleGeoClick(geo)} />
+                ))
+              }
+            </Geographies>
+          </ZoomableGroup>
         </ComposableMap>
       </div>
     </>
