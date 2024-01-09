9a4481e9595ebb0488b6ae719eb5e04de8699d85
Bryan McCarthy <bryanmccarthy2@gmail.com>
2024-01-09 10:45:04 +0000

    controls for zoom

--- src/App.tsx
+++ src/App.tsx
@@ -5,7 +5,6 @@ function App() {
   return (
     <>
       <div className="h-[calc(100dvh)]">
-        <div className="h-[calc(5dvh)] text-2xl font-bold text-center">Header</div>
         <World />
       </div>
     </>
--- src/components/World.tsx
+++ src/components/World.tsx
@@ -1,17 +1,39 @@
 import { ComposableMap, Geographies, Geography, ZoomableGroup } from 'react-simple-maps'
 import geoJSON from '../geo/countries.json'
+import { useState } from 'react'
+
+type Position = {
+  coordinates: [number, number],
+  zoom: number
+}
 
 export default function World() {
+  const [position, setPosition] = useState<Position>({ coordinates: [0, 0], zoom: 1 });
+
+  const handleZoomIn = () => {
+    if (position.zoom >= 4) return;
+    setPosition(pos => ({ ...pos, zoom: pos.zoom * 2 }));
+  }
+
+  const handleZoomOut = () => {
+    if (position.zoom <= 1) return;
+    setPosition(pos => ({ ...pos, zoom: pos.zoom / 2 }));
+  }
+
+  const handleMoveEnd = (position: any) => {
+    setPosition(position);
+  }
 
   const handleGeoClick = (geo: any) => {
-    console.log(geo)
+    console.log(geo);
   }
 
   return (
     <>
-      <div className="w-full h-[calc(95dvh)] overflow-scroll">
+      <div className="w-full h-full overflow-scroll">
+        <Controls handleZoomIn={handleZoomIn} handleZoomOut={handleZoomOut} />
         <ComposableMap className="bg-blue-500">
-          <ZoomableGroup center={[0, 0]} zoom={1}>
+          <ZoomableGroup zoom={position.zoom} center={position.coordinates} onMoveEnd={handleMoveEnd}>
             <Geographies geography={geoJSON}>
               {({ geographies }) =>
                 geographies.map((geo) => (
@@ -21,6 +43,22 @@ export default function World() {
             </Geographies>
           </ZoomableGroup>
         </ComposableMap>
+      </div>
+    </>
+  )
+}
+
+type ControlsProps = {
+  handleZoomIn: () => void,
+  handleZoomOut: () => void
+}
+
+function Controls({ handleZoomIn, handleZoomOut }: ControlsProps) {
+  return (
+    <>
+      <div className="absolute right-0 flex gap-2 p-2">
+        <button className="px-4 py-2 bg-black text-white rounded-md" onClick={handleZoomIn}>+</button>
+        <button className="px-4 py-2 bg-black text-white rounded-md" onClick={handleZoomOut}>-</button>
       </div>
     </>
   )
